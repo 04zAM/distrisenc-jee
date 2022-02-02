51b5dd84813b5d33b14698fb7f4fc63e5661bf31
CristianBT <cabrusilt@utn.edu.ec>
2022-02-02 07:42:52 -0500

    reporte

--- distrisencWeb/src/distrisenc/controller/inventario/BeanInvBodeguero.java
+++ distrisencWeb/src/distrisenc/controller/inventario/BeanInvBodeguero.java
@@ -1,10 +1,14 @@
 package distrisenc.controller.inventario;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -13,6 +17,8 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
 
 import org.primefaces.model.FilterMeta;
 import org.primefaces.model.MatchMode;
@@ -22,6 +28,9 @@ import distrisenc.model.core.entities.InventarioCa;
 import distrisenc.model.core.entities.PrdProducto;
 import distrisenc.model.inventario.managers.ManagerInventario;
 import distrisenc.model.produccion.managers.ManagerProduccion;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 
 
 
@@ -90,7 +99,33 @@ public class BeanInvBodeguero implements Serializable {
 		ListaProductos = listaProductos;
 	}
 
-	
+	public String actionReporte(){
+		Map<String,Object> parametros=new HashMap<String,Object>();
+		/*parametros.put("p_titulo_principal",p_titulo_principal);
+		parametros.put("p_titulo",p_titulo);*/
+		FacesContext context=FacesContext.getCurrentInstance();
+		ServletContext servletContext=(ServletContext)context.getExternalContext().getContext();
+		String ruta=servletContext.getRealPath("/inventario/bodeguero/reporteInv.jasper");
+		System.out.println(ruta);
+		HttpServletResponse response=(HttpServletResponse)context.getExternalContext().getResponse();
+		response.addHeader("Content-disposition", "attachment;filename=reporte.pdf");
+		response.setContentType("application/pdf");
+		try {
+		Class.forName("org.postgresql.Driver");
+		Connection connection = null;
+		connection = DriverManager.getConnection(
+		"jdbc:postgresql://localhost:5432/distrinsenc","postgres", "cristian");
+		JasperPrint impresion=JasperFillManager.fillReport(ruta, parametros,connection);
+		JasperExportManager.exportReportToPdfStream(impresion, response.getOutputStream());
+		context.getApplication().getStateManager().saveView ( context ) ;
+		System.out.println("reporte generado.");
+		context.responseComplete();
+		} catch (Exception e) {
+		JSFUtil.crearMensajeERROR(e.getMessage());
+		e.printStackTrace();
+		}
+		return "";
+		}
    
 	
 	
